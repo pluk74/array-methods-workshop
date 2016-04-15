9c42da1cd866a6da2825f3565439dba09ff80cf1
pluk74 <pluk74@hotmail.com>
2016-04-15 17:58:29 +0000

    Exercise 2: getPositives

--- answers.js
+++ answers.js
@@ -24,3 +24,20 @@ var x = [-5,-67,-55,1,2,3,4,5,6,7,8,9,10,-333];
 //console.log(printPositives(x));
 
 //console.log(retPos(2));
+
+/*
+Exercise 2
+
+Similar to the previous exercise, write a function called getPositives that takes an array and uses its filter method to return a new array with only the positive numbers
+*/
+
+function getPositives (arr) {
+    
+    return arr.filter(retPositives);
+}
+
+function retPositives (num) {
+    return num >= 0;
+}
+
+//console.log(getPositives(x));
