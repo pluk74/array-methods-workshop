cca1392260b2c4095f58c90595d2a15b87405905
pluk74 <pluk74@hotmail.com>
2016-04-15 19:35:56 +0000

    Exercise 8: countChars

--- answers.js
+++ answers.js
@@ -222,5 +222,111 @@ function findHighTwolowTwo (prev,curr) {
     return prev;
 }
 
-var numArr = [1, -10, 20, 40, 5];
-console.log(highLowTwo(numArr));
+//var numArr = [1, -10, 20, 40, 5];
+//console.log(highLowTwo(numArr));
+
+/*
+Exercise 8
+
+Write a function called countChars that takes a string, and returns an object where the keys are letters, and the value is the number of times that letter appears.
+
+For example, with input "hello world", the output should be:
+
+{
+  "h": 1,
+  "e": 1,
+  "l": 3,
+  "o": 2,
+  "w": 1,
+  "r": 1,
+  "d": 1
+}
+NOTE: Unlike arrays, objects don’t have any ordering on them. When you print your object on the console, your keys may be displayed in a different order, and it does not matter.
+*/
+
+function countChars (str) {
+    var obj = {};
+    var arr = myStr.replace(" ","").split("");
+    
+    return arr.reduce(tallyChars,{});
+}
+
+function tallyChars (tally,curr) {
+    
+    if(!tally[curr]) {
+        tally[curr] = 1;
+    }
+    else {
+        tally[curr] = tally[curr] +1;
+    }
+    return tally;
+}
+
+var myStr = "hello world";
+console.log(countChars(myStr));
+
+
+
+/*
+Exercise 9
+
+Starting on week 3 of the bootcamp, we will be learning about databases. Databases are useful for saving data for long periods of time.
+
+Contrary to a JavaScript program, where all the data disappears when the program terminates, databases keep their data "forever".
+
+In this exercise, we’re going to imagine that we are storing people information in a database, and that we queried our database to retrieve a list of people.
+
+Our database returns to us an array of people objects, and each of them has a unique ID that the database uses to refer to it.
+
+Here’s what our person data could look like:
+
+[
+  {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+]
+For this exercise, we want to use Array.prototype.reduce to transform our array of people into an object, keyed with the unique ID.
+
+The end result should look like this:
+
+{
+  "KeXoYg92is": {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  "NkALmSWtUp": {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  "m7LPbJYSUg": {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+}
+This object could be useful if we are often looking up people by their unique ID.
+
+Write a function called peopleById that takes an array of people and returns an object where each person is keyed by their unique ID.
+
+You have effectively created what we call an index, not unlike the one you have in your phonebook.
+*/
